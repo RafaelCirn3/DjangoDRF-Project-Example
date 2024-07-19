078f34350cedd4804b8c87dbc01cc922e280d221
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2024-07-19 16:43:46 -0300

    :arrow_up: novas adições ao model Arma, com uma variável do tipo Choices

--- apps/arma/models.py
+++ apps/arma/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
+# Definindo as escolhas para o tipo de arma
+TIPO_ARMA_CHOICES = [
+    ('eletrico', 'Elétrico'),
+    ('fogo', 'Fogo'),
+    ('agua', 'Água'),
+    ('sombrio', 'Sombrio'),
+    ('neutro', 'Neutro'),
+    ('solar', 'Solar'),
+]
+
 # Definindo o modelo Arma
 class Arma(models.Model):
     # Campo de nome da arma
     nome = models.CharField(max_length=100)
     # Campo de poder da arma (um inteiro representando a força da arma)
     nivel = models.IntegerField()
+    elemento = models.CharField(max_length=10, choices=TIPO_ARMA_CHOICES, default='neutro') 
 
     # Método que retorna uma string representando o objeto
     def __str__(self):
-        return f'{self.nome}, Nível: {self.nivel}'
+        return f'{self.nome}, Nível: {self.nivel}, Elemento: {self.elemento}'
