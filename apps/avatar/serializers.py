4638df5c5ebfc58ff672d45daad5005e92cc06c9
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2024-07-19 16:38:32 -0300

    :arrow_up: validação para o avatar não repetir

--- apps/avatar/serializers.py
+++ apps/avatar/serializers.py
@@ -8,3 +8,15 @@ class AvatarSerializer(serializers.ModelSerializer):
         model = Avatar
         # Especifica os campos a serem incluídos na serialização
         fields = '__all__'
+    
+    def validate(self, sttrs):
+    # Pega o nickname do avatar
+        nickname = sttrs.get('nickname')
+
+        # Verifica se já existe um avatar com o mesmo nickname
+        if Avatar.objects.filter(nickname=nickname).exists():
+            # Se existe, lança uma validação de erro
+            raise serializers.ValidationError(f"Um avatar com o nome {nickname} já existe.")
+
+        # Se o nickname for único, chama o método validate da superclasse
+        return super().validate(sttrs)
