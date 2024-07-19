59e2b66b40ec9956e350ea5d7ea65d0b551a13a8
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2024-07-19 16:49:32 -0300

    :arrow_up: novos campos incluindo outro choices, e melhor definição do objeto Arma

--- apps/arma/models.py
+++ apps/arma/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Definindo as escolhas para o tipo de arma
+# Definindo as escolhas para o elemento da arma
 TIPO_ARMA_CHOICES = [
     ('eletrico', 'Elétrico'),
     ('fogo', 'Fogo'),
@@ -10,14 +10,26 @@ TIPO_ARMA_CHOICES = [
     ('solar', 'Solar'),
 ]
 
+# Definindo as escolhas para o tipo de arma
+TIPO_CHOICES = [
+    ('espada', 'Espada'),
+    ('machado', 'Machado'),
+    ('arco', 'Arco'),
+    ('foice', 'Foice'),
+    ('lanca', 'Lança'),
+]
+
 # Definindo o modelo Arma
 class Arma(models.Model):
     # Campo de nome da arma
     nome = models.CharField(max_length=100)
     # Campo de poder da arma (um inteiro representando a força da arma)
     nivel = models.IntegerField()
-    elemento = models.CharField(max_length=10, choices=TIPO_ARMA_CHOICES, default='neutro') 
+    # Campo de elemento da arma com choices
+    elemento = models.CharField(max_length=10, choices=TIPO_ARMA_CHOICES, default='neutro')
+    # Campo de tipo da arma com choices
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES, default='espada')
 
     # Método que retorna uma string representando o objeto
     def __str__(self):
-        return f'{self.nome}, Nível: {self.nivel}, Elemento: {self.elemento}'
+        return f'{self.nome}, Nível: {self.nivel}, Elemento: {self.elemento}, Tipo: {self.tipo}'
